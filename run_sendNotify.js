36de9bcbf4c6b677763f19704c1c12b105a4dcaa
tracefish <64358864+tracefish@users.noreply.github.com>
2021-04-07 17:05:49 +0800

    Update run_sendNotify.js

--- run_sendNotify.js
+++ run_sendNotify.js
@@ -1,6 +1,6 @@
 notify = require('./sendNotify');
 fs = require('fs');
-var data = fs.readFileSync('./dt.conf');
-var name = fs.readFileSync('./dt.confname');
+var data = fs.readFileSync('../dt.conf');
+var name = fs.readFileSync('../dt.confname');
 
 notify.sendNotify(name, data.toString());
