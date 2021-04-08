1968722b77a44956514e348fba4fc96335e75219
tracefish <64358864+tracefish@users.noreply.github.com>
2021-04-08 11:44:39 +0800

    Update concurrency_timer.sh

--- concurrency_timer.sh
+++ concurrency_timer.sh
@@ -8,6 +8,16 @@ sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 cd ~/scripts
+
+if [ -n "$SYNCURL" ]; then
+    echo "下载脚本"
+    curl "$SYNCURL" > "./$1"
+    # 外链脚本替换
+    sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
+    sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
+fi
+[ ! -e "./$1" ] && echo "脚本不存在" && exit 0
+
 echo "DECODE"
 encode_str=(`cat ./$1 | grep "window" | awk -F "window" '{print($1)}'| awk -F "var " '{print $(NF-1)}' | awk -F "=" '{print $1}' | sort -u`)
 if [ -n "$encode_str" ]; then
@@ -24,6 +34,11 @@ num=0
 echo "修改cookie"
 sed -i 's/process.env.JD_COOKIE/process.env.JD_COOKIES/g' ./jdCookie.js
 JK_LIST=(`echo "$JD_COOKIE" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
+
+echo "修改发送方式"
+cp -f ./sendNotify.js ./sendNotify_diy.js
+sed -i "s/desp += author/\/\/desp += author/g" ./sendNotify.js
+sed -i "/text = text.match/a   var fs = require('fs');fs.appendFile(\"./\" + \"$NOTIFY_CONF\", text + \"\\\n\", function(err) {if(err) {return console.log(err);}});fs.appendFile(\"./\" + \"$NOTIFY_CONF\", desp + \"\\\n\", function(err) {if(err) {return console.log(err);}});\n  return" ./sendNotify.js
 
 num=0
 for jk in ${JK_LIST[*]}
@@ -45,3 +60,60 @@ do
 done
 unset IFS
 wait
+
+#判断是否需要特别推送
+specify_send(){
+  ret=`cat $1 | grep "提醒\|已超时\|已可兑换"`
+  [ -n "$ret" ] && echo 1 || echo 0
+}
+
+# 清空文件
+echo "" > ~/${NOTIFY_CONF}
+echo "" > ~/${NOTIFY_CONF}spec
+# 整合推送消息和助力码
+for n in `seq 1 ${#JK_LIST[*]}`
+do
+    cd ~/scripts${n}
+    if [ -e "./${NOTIFY_CONF}" ]; then
+        if [ $(specify_send ./${NOTIFY_CONF}) -eq 0 ];then
+            echo "" >> ~/${NOTIFY_CONF}
+            cat ./${NOTIFY_CONF}  | tail -n +2 | sed "s/账号[0-9]/账号$n/g" >> ~/${NOTIFY_CONF}
+        else
+            echo "" >> ~/${NOTIFY_CONF}spec
+            cat ./${NOTIFY_CONF}  | tail -n +2 | sed "s/账号[0-9]/账号$n/g" >> ~/${NOTIFY_CONF}spec
+        fi
+    fi
+    [ -e "./${NOTIFY_CONF}" -a ! -e "~/${NOTIFY_CONF}name" ] && cat ./${NOTIFY_CONF} | head -n 1 > ~/${NOTIFY_CONF}name 
+done
+
+cd ~/scripts
+echo "推送消息"
+[ "$(cat ~/${NOTIFY_CONF}name | tail -n 1)"x = ""x ] && sed -i '$d' ~/${NOTIFY_CONF}name
+if [ -e ~/${NOTIFY_CONF} ]; then
+    # 删除连续空行为一行
+    sed -i '/^$/{N;/\n$/d}' ~/${NOTIFY_CONF}
+    #清除文首文末空行
+    [ "$(cat ~/${NOTIFY_CONF} | head -n 1)"x = ""x ] && sed -i '1d' ~/${NOTIFY_CONF}
+    [ "$(cat ~/${NOTIFY_CONF} | tail -n 1)"x = ""x ] && sed -i '$d' ~/${NOTIFY_CONF}
+    cp -f ./sendNotify_diy.js ./sendNotify.js
+    sed -i "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js
+    node ./run_sendNotify.js
+    rm -f ./${NOTIFY_CONF}
+    rm -f ~/${NOTIFY_CONF}
+fi
+# 特殊推送
+if [ -e ~/${NOTIFY_CONF}spec ]; then
+    # 删除连续空行为一行
+    sed -i '/^$/{N;/\n$/d}' ~/${NOTIFY_CONF}spec
+    #清除文首文末空行
+    [ "$(cat ~/${NOTIFY_CONF}spec | head -n 1)"x = ""x ] && sed -i '1d' ~/${NOTIFY_CONF}spec
+    [ "$(cat ~/${NOTIFY_CONF}spec | tail -n 1)"x = ""x ] && sed -i '$d' ~/${NOTIFY_CONF}spec
+    cp -f ./sendNotify_diy.js ./sendNotify.js
+    sed -i "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js
+    sed -i "s/process.env.DD_BOT_TOKEN/process.env.DD_BOT_TOKEN_SPEC/g" ./sendNotify.js
+    sed -i "s/process.env.DD_BOT_SECRET/process.env.DD_BOT_SECRET_SPEC/g" ./sendNotify.js
+    node ./run_sendNotify_spec.js
+    rm -f ~/${NOTIFY_CONF}spec
+fi
+# 恢复原文件
+cp -f ./sendNotify_diy.js ./sendNotify.js
