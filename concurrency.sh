f15665edc7829ed71e3b99209627f29d987fc4a0
tracefish <64358864+tracefish@users.noreply.github.com>
2021-04-10 08:22:57 +0800

    Update concurrency.sh

--- concurrency.sh
+++ concurrency.sh
@@ -190,14 +190,14 @@ rm -f ~/${LOG}
 for n in `seq 1 ${#JK_LIST[*]}`
 do
     cd ~/scripts${n}
-    [ -e "./${LOG}1" ] && cat ./${LOG}1  | sed "s/账号[0-9]/账号$n/g" >> ~/${LOG}
+    [ -e "./${LOG}1" ] && cat ./${LOG}1  | sed -e "s/账号[0-9]/账号$n/g" "s/京东号[0-9]/京东号$n/g" >> ~/${LOG}
     if [ -e "./${NOTIFY_CONF}" ]; then
         echo "" >> ~/${NOTIFY_CONF}
         echo "" >> ~/${NOTIFY_CONF}name
         if [ $(specify_send ./${NOTIFY_CONF}) -eq 0 ];then
-            cat ./${NOTIFY_CONF}  | tail -n +2 | sed "s/账号[0-9]/账号$n/g" >> ~/${NOTIFY_CONF}
+            cat ./${NOTIFY_CONF}  | tail -n +2 | sed -e "s/账号[0-9]/账号$n/g" "s/京东号[0-9]/京东号$n/g" >> ~/${NOTIFY_CONF}
         else
-            cat ./${NOTIFY_CONF}  | tail -n +2 | sed "s/账号[0-9]/账号$n/g" >> ~/${NOTIFY_CONF}spec
+            cat ./${NOTIFY_CONF}  | tail -n +2 | sed -e "s/账号[0-9]/账号$n/g" "s/京东号[0-9]/京东号$n/g" >> ~/${NOTIFY_CONF}spec
         fi
         [ ! -s "~/${NOTIFY_CONF}name" ] && cat ./${NOTIFY_CONF} | head -n 1 > ~/${NOTIFY_CONF}name 
     fi
@@ -212,14 +212,18 @@ if [ -e ~/${NOTIFY_CONF} ]; then
     blank_lines2blank_line ~/${NOTIFY_CONF}
     blank_lines2blank_line ~/${NOTIFY_CONF}name
     cp -f ./sendNotify_diy.js ./sendNotify.js
-    sed -e 's/text}\\n\\n/text}\\n/g' 's/\\n\\n本脚本/\\n本脚本/g' "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js > ./sendNotify.js
+    sed -i 's/text}\\n\\n/text}\\n/g' ./sendNotify.js
+    sed -i 's/\\n\\n本脚本/\\n本脚本/g' ./sendNotify.js
+    sed -i  "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js
     node ./run_sendNotify.js
 fi
 # 特殊推送
 if [ -e ~/${NOTIFY_CONF}spec ]; then
     blank_lines2blank_line ~/${NOTIFY_CONF}spec
     cp -f ./sendNotify_diy.js ./sendNotify.js
-    sed -e 's/text}\\n\\n/text}\\n/g' 's/\\n\\n本脚本/\\n本脚本/g' "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js > ./sendNotify.js
+    sed -i 's/text}\\n\\n/text}\\n/g' ./sendNotify.js
+    sed -i 's/\\n\\n本脚本/\\n本脚本/g' ./sendNotify.js
+    sed -i  "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js
     sed -i "s/process.env.DD_BOT_TOKEN/process.env.DD_BOT_TOKEN_SPEC/g" ./sendNotify.js
     sed -i "s/process.env.DD_BOT_SECRET/process.env.DD_BOT_SECRET_SPEC/g" ./sendNotify.js
     node ./run_sendNotify_spec.js
