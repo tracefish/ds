04bad539b50f54bb1b7d5db51f0b96c88fe054fa
tracefish <64358864+tracefish@users.noreply.github.com>
2021-04-15 17:09:23 +0800

    Update concurrency.sh

--- concurrency.sh
+++ concurrency.sh
@@ -7,7 +7,7 @@
 # 通过 `test.sh jd.js delay 2` 指定延迟时间
 # `test.sh jd.js 00:00:12 2` 通过时间，指定脚本 运行时间 和 延迟时间（默认为0）
 #  `test.sh jd.js 12 2` 通过分钟（小于等于十分钟，需要设置定时在上一个小时触发），指定脚本 运行时间 和 延迟时间（默认为0）
-# 版本：v2.2
+# 版本：v2.3
 
 # set -e
 SCRIPT="$1"
@@ -258,13 +258,13 @@ main(){
 	sed -i 's/\\n\\n本脚本/\\n本脚本/g' ./sendNotify.js
 	sed -i  "s/text = text.match/\/\/text = text.match/g" ./sendNotify.js
 
-	if [ -e ${home}/${NOTIFY_CONF} -a -s ${home}/${NOTIFY_CONF} ]; then
+	if [ -e ${home}/${NOTIFY_CONF} -a -n `cat ${home}/${NOTIFY_CONF} | sed '/^$/d'` ]; then
 		blank_lines2blank_line ${home}/${NOTIFY_CONF}
 		blank_lines2blank_line ${home}/${NOTIFY_CONF}name
 		node ./run_sendNotify.js
 	fi
 	# 特殊推送
-	if [ -e ${home}/${NOTIFY_CONF}spec -a -s ${home}/${NOTIFY_CONF}spec ]; then
+	if [ -e ${home}/${NOTIFY_CONF}spec -a -n `cat ${home}/${NOTIFY_CONF}spec | sed '/^$/d'` ]; then
 		blank_lines2blank_line ${home}/${NOTIFY_CONF}spec
 		sed -i "s/process.env.DD_BOT_TOKEN/process.env.DD_BOT_TOKEN_SPEC/g" ./sendNotify.js
 		sed -i "s/process.env.DD_BOT_SECRET/process.env.DD_BOT_SECRET_SPEC/g" ./sendNotify.js
