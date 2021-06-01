d33ea5658093824bb8d1abae196a4be3b56f9ab2
tracefish <64358864+tracefish@users.noreply.github.com>
2021-06-01 20:31:33 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -6,7 +6,7 @@ git config --global user.name "github-actions[bot]"
 sudo git clone https://github.com/tracefish/ds ~/tmp_ds
 sudo mkdir ~/tmp_scripts/
 sudo mv -f ~/tmp_ds/*.sh ~/tmp_scripts/
-sudo mv -f ~/tmp_ds/*.js ~/tmp_scripts/
+#sudo mv -f ~/tmp_ds/*.js ~/tmp_scripts/
 
 get_by_git(){
     mkdir -p ~/jd_scripts/logs
