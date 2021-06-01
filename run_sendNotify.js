3512b3ff9fc870bfd5a5aa66624c0693317aecc9
tracefish <64358864+tracefish@users.noreply.github.com>
2021-06-01 22:36:39 +0800

    Delete run_sendNotify.js

--- run_sendNotify.js
+++
@@ -1,6 +0,0 @@
-notify = require('./sendNotify');
-fs = require('fs');
-var data = fs.readFileSync('../dt.conf');
-var name = fs.readFileSync('../dt.confname');
-
-notify.sendNotify(name, data.toString());
